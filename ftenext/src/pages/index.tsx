a4dfeff8468bb62b1c4ddc1e6a730fa688c89eb7
alanpugachev <aszneesizzle@gmail.com>
2023-05-14 21:31:36 +0300

    Add json to index.tsx

--- ftenext/src/pages/index.tsx
+++ ftenext/src/pages/index.tsx
@@ -1,16 +1,44 @@
 import { Inter } from 'next/font/google';
 import Layout from '../../components/Layout/Layout';
+import { useEffect, useState } from 'react';
+import jwt from 'jsonwebtoken';
 
 const inter = Inter({ subsets: ['latin'] });
+
+const isUserLoggedIn = () => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return false;
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'secret-key');
+    return true;
+  } catch (err) {
+    console.error('Invalid token', err);
+    return false;
+  }
+};
 
 interface HomePageProps {
 }
 
 const HomePage: React.FC<HomePageProps> = ({}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setIsLoggedIn(isUserLoggedIn());
+  }, []);
+
+  if(!isLoggedIn) {
+    <Layout pageTitle="HourlyHub">
+      <h1>Welcome to HourlyHub. Please log in or register</h1>
+    </Layout>
+  }
+
   return (
     <Layout pageTitle="HourlyHub">
       <h1>Welcome to HourlyHub</h1>
-      <p>If you are not authorized, please log in to your account</p>
     </Layout>
   );
 };
