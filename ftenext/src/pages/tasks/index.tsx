f7c7db675655254724ea7f2c945e3a2118345367
alanpugachev <aszneesizzle@gmail.com>
2023-05-30 21:29:08 +0300

    ... 2

--- ftenext/src/pages/tasks/index.tsx
+++ ftenext/src/pages/tasks/index.tsx
@@ -1,62 +1,52 @@
 import { Inter } from 'next/font/google';
 import Layout from '../../../components/Layout/Layout';
-import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useEffect, useState } from 'react';
+import { useState } from 'react';
 import styles from './Tasks.module.scss';
 
 interface TasksPageProps {
 }
 
-const TasksPage: React.FC<TasksPageProps> = ({}) => {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    (
-      async () => {
-        try {
-          const userResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user`, {
-            method: 'GET',
-            headers: {'Content-Type': 'application/json'},
-            credentials: 'include',
-          });
-
-          const userContent = await userResponse.json();
-          //setMessage(`${userContent}`);
-          console.log(userContent)
-
-          const tasksResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/db/task/${userContent.secondName}`, {
-            method: 'GET',
-            headers: {'Content-Type': 'application/json'},
-            credentials: 'include',
-          });
-
-          const tasksContent = await tasksResponse.json();
-          //setMessage(`Tasks: ${tasksContent[0].title}`);
-          console.log(tasksContent);
-
-          const listOfTasks = tasksContent.map(
-            (element: { title: string; price: string; }) => {
-              return (
-                <ul>
-                  {element.title}
-                  {element.price}
-                </ul>
-              )
-            }
-          )
-
-          setMessage(`${listOfTasks}`)
-        } catch(e) {
-          setMessage('There is no tasks to return')
-        }
-      }
-    )();
-  });
-
+const TasksPage: React.FC<TasksPageProps> = ({ tasks }) => {
   return (
     <Layout pageTitle='My Tasks'>
-      {message}
+      
     </Layout>
   );
 };
+
+export const getStaticProps = async (context: any) => {
+  const [message, setMessage] = useState('');
+
+  async () => {
+    try {
+      const userResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include',
+      });
+
+      const userContent = await userResponse.json();
+      //setMessage(`${userContent}`);
+      console.log(userContent)
+
+      const tasksResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/db/task/${userContent.secondName}`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include',
+      });
+
+      const tasks = await tasksResponse.json();
+      console.log(tasks);
+
+      return {
+        props: {
+          tasks,
+        },
+      };
+    } catch(e) {
+      setMessage('There is no tasks to return')
+    }
+  }
+}
 
 export default TasksPage;
