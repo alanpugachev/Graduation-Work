82c53fe89c874c764ffc72f17bcc58243fad844c
alanpugachev <aszneesizzle@gmail.com>
2023-06-02 19:58:20 +0300

    Add remove handler

--- ftenext/src/pages/tasks/index.tsx
+++ ftenext/src/pages/tasks/index.tsx
@@ -9,17 +9,27 @@ interface TasksPageProps {
 interface Task {
   id: number,
   title: string,
-  execution_time: string,
+  executionTime: string,
   customer: string,
   price: string
 }
 
-interface TasksArray {
-  tasksArr: Array<Task>
-}
-
 const TasksPage: React.FC<TasksPageProps> = ({}) => {
   const [task, setTask] = useState<any>();
+
+  async function handleRemove(id: number) {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/db/delete/${id}`, {
+        method: 'POST'
+      });
+
+      const newTasks = task.filter((item: {id: number}) => item.id !== id);
+
+      setTask(newTasks);
+    } catch(e) {
+      console.log(`Error when deleting item: ${id}`)
+    }
+  }
 
   useEffect(() => {
     (
@@ -52,9 +62,33 @@ const TasksPage: React.FC<TasksPageProps> = ({}) => {
 
   return (
     <Layout pageTitle='My Tasks'>
-      <h3>
-        {task?.map((item: { title: string | null | undefined; }) => <div>{item.title}</div>)}
-      </h3>
+      <div>
+        {task?.map((item: { 
+          id: number;
+          title: string | null | undefined; 
+          price: string | null | undefined;
+          executionTime: string | null | undefined;
+          }) => 
+          <div>
+            <ul>
+              <h1>{item.title}</h1>
+              <h2>{item.executionTime}</h2>
+              <h3>{item.price}</h3>
+            </ul>
+            <button>Edit</button>
+            <br />
+            <button type='button' onClick={() => handleRemove(item.id)}>
+              Delete
+            </button>
+          </div>
+          )}
+      </div>
+
+      <div>
+        <button type='button'>
+          Add Task
+        </button>
+      </div>
     </Layout>
   );
 };
